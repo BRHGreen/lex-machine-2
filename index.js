bb3240eabb59d2baac20f23bd1a37e5a1e869b8b
BRHGreen <benedictgreen@gmail.com>
2017-10-14 17:48:48 +0100

    add user mustation working server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import { graphiqlExpress, graphqlExpress } from 'graphql-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import models from './models'
+import cors from 'cors'
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
@@ -14,16 +15,18 @@ const schema = makeExecutableSchema({
   resolvers,
 });
 
+app.use(cors('*'))
+
 app.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
 }));
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema, context: { models } }));
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// const webpackMiddleware = require('webpack-dev-middleware');
+// const webpack = require('webpack');
+// const webpackConfig = require('./webpack.config.js');
+// app.use(webpackMiddleware(webpack(webpackConfig)));
 
 // creates/syncs the database
 models.sequelize.sync().then(() => app.listen(3000));
