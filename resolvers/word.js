ba713296cb7eb7a5b22a28915bd8b4bb32ef6f25
BRHGreen <benedictgreen@gmail.com>
2018-01-01 13:50:18 +0000

    editting all fields on words

--- resolvers/word.js
+++ resolvers/word.js
@@ -3,12 +3,21 @@ import requiresAuth from '../permissions';
 
 export default {
   Query: {
-    getWord: (parent, { id }, { models }) => models.Word.findOne({ where: { id } })
+    getWord: (parent, { id }, { models }) => models.Word.findOne({ where: { id } }),
   },
   Mutation: {
     deleteWord: (parent, args, { models }) =>
       models.Word.destroy({ where: args }),
-    updateWord: (parent, { id, newWord }, { models }) => models.Word.update({ word: newWord }, { where: { id } }),
+    updateWord: (parent, {
+      id,
+      newWord,
+      newPartOfSpeach,
+      newDefinition,
+    }, { models }) => models.Word.update({
+      word: newWord,
+      partOfSpeach: newPartOfSpeach,
+      definition: newDefinition,
+    }, { where: { id } }),
     createWord: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
         await models.Word.create({ ...args, owner: user.id });
